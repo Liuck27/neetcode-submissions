30867c8850f268f5038c2f6de2fcf6d96eb3eec6
Luca Secchieri <132589644+Liuck27@users.noreply.github.com>
2026-06-28 13:41:38 +0200

    Add: design-word-search-data-structure - submission-3

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-3.py
@@ -0,0 +1,35 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
+
+
+class WordDictionary:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                node.children[c] = TrieNode()
+            node = node.children[c]
+        node.endOfWord = True
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        n = len(word)
+
+        def dfs(i, node):
+            if i == n:
+                return node.endOfWord
+            c = word[i]
+            res = False 
+            if c in node.children:
+                res = res or dfs(i + 1, node.children[c])
+            elif c == ".":
+                for child in node.children:
+                    res = res or dfs(i + 1, node.children[child])
+            return res
+
+        return dfs(0, node)
