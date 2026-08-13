99920deaaf42f135b9718130abc87ef03fd88e72
Luca Secchieri <132589644+Liuck27@users.noreply.github.com>
2026-08-13 23:10:19 +0200

    Add: sql-customers-bought-a-b-not-c - submission-1

---
+++ SQL/sql-customers-bought-a-b-not-c/submission-1.sql
@@ -0,0 +1,7 @@
+-- Fast because execution short-circuits as soon as a match is found
+SELECT c.customer_id, c.customer_name
+FROM customers c
+WHERE EXISTS (SELECT 1 FROM orders WHERE customer_id = c.customer_id AND product_name = 'A')
+  AND EXISTS (SELECT 1 FROM orders WHERE customer_id = c.customer_id AND product_name = 'B')
+  AND NOT EXISTS (SELECT 1 FROM orders WHERE customer_id = c.customer_id AND product_name = 'C')
+ORDER BY c.customer_name
